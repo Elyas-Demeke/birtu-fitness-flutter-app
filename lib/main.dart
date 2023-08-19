d0206403bed61d50452acfcc0f590854da555803
Elyas-Demeke <nebiyou.elyas11@gmail.com>
2023-08-19 15:44:00 +0300

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,220 @@
+import 'dart:ffi';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {
+void main(List<String> args) {
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+final List<String> entries = <String>['A', 'B', 'C'];
+final List<int> colorCodes = <int>[600, 500, 100];
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int count = 20, currentPageIndex = 0;
+  List<String> titles = ['ስልጠናዎች','የራስዎ','እንቅስቃሴዎች','ሪፖርት','እኔ'];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    count = 10;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      theme: myappTheme,
+      home: Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            title: Text(titles[currentPageIndex], style: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.w700),)),
+            // body: ListView.builder(
+            //   padding: EdgeInsets.all(0),
+            //   itemCount: 3,
+            //   itemBuilder: (_, index) {
+            //     return Container(
+            //       color: Colors.amber[colorCodes[index]],
+            //       child: Center(
+            //         child: Text('Entry ${entries[index]}'),
+            //       ),
+            //       width:  500,
+            //       height: 500,
+            //     );
+            //   },
+            // ),
+        // body: ListView(
+        //   scrollDirection: Axis.horizontal,
+        //   children: [
+        //     Container(
+        //       color: Colors.green,
+        //       width: 720,
+        //       height: 200,
+        //     ),
+        //     Container(
+        //       color: Colors.yellow,
+        //       width: 1000,
+        //       height: 200,
+        //     ),
+        //     Container(
+        //       color: Colors.red,
+        //       width: 1000,
+        //       height: 200,
+        //     ),
+        //     Positioned(
+        //       child: Icon(Icons.verified),
+        //       top: 25,
+        //       left: 25,
+        //     ),
+        //   ],
+        // ),
+      body: [ElevatedButton(
+        child: Text(titles[currentPageIndex]),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => AboutScreen(),
+              ) 
+          );
+        },
+      ), ElevatedButton(
+        child: Text('Second'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => AboutScreen(),
+              ) 
+          );
+        },
+      ),ElevatedButton(
+        child: Text('third'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => AboutScreen(),
+              ) 
+          );
+        },
+      ),ElevatedButton(
+        child: Text('fourth'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => AboutScreen(),
+              ) 
+          );
+        },
+      ),ElevatedButton(
+        child: Text('fifth'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => AboutScreen(),
+              ) 
+          );
+        },
+      )][currentPageIndex],        
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              count++;
+            });
+          },
+        ),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          child: NavigationBar(
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.home), 
+              label: 'ስልጠናዎች',
+              selectedIcon: Icon(Icons.home, color: Colors.blue[600]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            NavigationDestination(
+              icon: Icon(Icons.brush_sharp), 
+              label: 'የራስዎ',
+              selectedIcon: Icon(Icons.brush_sharp, color: Colors.blue[600],),
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.schema), 
+              label: 'እንቅስቃሴዎች',
+              selectedIcon: Icon(Icons.schema, color: Colors.blue[600]),
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.bar_chart), 
+              label: 'ሪፖርት',
+              selectedIcon: Icon(Icons.bar_chart, color: Colors.blue[600]),
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person), 
+              label: 'እኔ',
+              selectedIcon: Icon(Icons.person, color: Colors.blue[600]),
             ),
           ],
+          selectedIndex: currentPageIndex,
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          indicatorColor: Colors.transparent,
+          animationDuration: Duration(milliseconds: 150),
+          // labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          backgroundColor: Colors.white,
+        )),
+        drawer: const Drawer(
+          child: Text('Yo!'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({Key? key}): super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text('Flutter is fun!'),
+      ),
+      body: ElevatedButton(
+        child: Text('MyApp'),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => MyApp(),
+              ) 
+          );
+        },
+      ),
+    );
+  }
+}
+final ThemeData myappTheme = ThemeData(
+  navigationBarTheme: NavigationBarThemeData(
+    labelTextStyle: MaterialStateProperty.resolveWith((state) {
+      if (state.contains(MaterialState.selected)) {
+        return const TextStyle(color: Colors.blue);
+      }
+      return const TextStyle(color: Colors.black);
+    }),
+  ),
+);
